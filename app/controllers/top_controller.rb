bd985dd020dd5b557d30858f3c783a6fe6b375a8
yuhikoizumi <ykoizumi@iacc.co.jp>
2017-11-13 14:44:13 +0900

    Delete Sleep

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -5,7 +5,6 @@ class TopController < ApplicationController
     page = agent.get('https://www.j-anshin.co.jp/list_todokede/search_list.php?mode=hokenbetsu&todoufuken=&trader=&reform_flg=1&p=1')
     @data = page.search('.ulDotList li p').inner_text
     puts @data.first
-    sleep(20)
   end
 
   def new
