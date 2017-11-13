0d01743b19f811dc507abe9876e28db64afb04ef
yuhikoizumi <ykoizumi@iacc.co.jp>
2017-11-13 14:32:46 +0900

    Confirmation

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,12 +2,10 @@ class TopController < ApplicationController
   require 'mechanize'
   def index 
     agent = Mechanize.new
-    # (1..141).each do |i|
-    #   page = agent.get("https://www.j-anshin.co.jp/list_todokede/search_list.php?mode=hokenbetsu&todoufuken=&trader=&reform_flg=1&p=#{i}").click
-        # time.sleep(10)
-    #   page = agent.get("https://www.j-anshin.co.jp/list_todokede/search_list.php?mode=hokenbetsu&todoufuken=&trader=&reform_flg=1&p=#{i + 1}").click
-    # end
-    @data = page.search('.ulDotList li p')
+    page = agent.get('https://www.j-anshin.co.jp/list_todokede/search_list.php?mode=hokenbetsu&todoufuken=&trader=&reform_flg=1&p=1')
+    @data = page.search('.ulDotList li p').inner_text
+    puts @data.first
+    sleep(20)
   end
 
   def new
